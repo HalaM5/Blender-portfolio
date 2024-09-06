87a684462e7443548d8a465e9446fa6066242fa7
HalaM5 <hala_mm_5@yahoo.com>
2024-09-06 18:35:04 +0300

    modify css and html

--- js/threejs.js
+++ js/threejs.js
@@ -3,7 +3,7 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 
-
+//document.getElementById
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 1000 );
 camera.position.set(4, 5, 11);
